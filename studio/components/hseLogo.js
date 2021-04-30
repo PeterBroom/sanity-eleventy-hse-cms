b44d284f726dc17d62617086640455ec017e021c
Peter Broom <me@peterbroom.co.uk>
2021-04-30 16:50:56 +0100

    add logo component

---
+++ studio/components/hseLogo.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './hseLogo.css';
+
+const hseLogo = () => (
+  <img className={styles.hseLogo} src="/static/logo/hse-logo.png" alt="HSE logo"/>
+);
+
+export default hseLogo
