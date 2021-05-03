c924bd63691c60fc94cd658ddea33646138c1491
Peter Broom <me@peterbroom.co.uk>
2021-05-03 16:03:15 +0100

    fix logo

--- studio/components/hseLogo.js
+++ studio/components/hseLogo.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styles from './hseLogo.css';
 
 const hseLogo = () => (
-  <img className={styles.hseLogo} src="/static/logo/hse-logo.png" alt="HSE logo"/>
+  <img className={styles.hseLogo} src="/static/logo/hse-logo.svg" alt="HSE logo"/>
 );
 
 export default hseLogo
