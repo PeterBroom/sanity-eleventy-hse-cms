370e5697692fdc25455876e02a29db0558d55a4c
Peter Broom <me@peterbroom.co.uk>
2021-05-17 11:04:16 +0100

    subnav home

---
+++ web/_data/subnavHomepage.js
@@ -0,0 +1,46 @@
+const groq = require('groq')
+const client = require('../utils/sanityClient.js')
+const overlayDrafts = require('../utils/overlayDrafts')
+const hasToken = !!client.config().token
+
+function generateMenu (page) {
+  return {
+    ...page,
+  }
+}
+
+async function navigationTree () {
+  const filter = groq`*[_type == "homepage" && defined(slug) && _updatedAt < now()]`
+  const projection = groq`{
+      slug,
+      title,
+      shortTitle,
+      subMenu[]{
+        ...,
+        sections[]{
+          ...,
+          target->{title, shortTitle, slug, _id},
+          links[]{
+            ...,
+            target->{title, shortTitle, slug, _id},
+            children[]{
+              ...,
+              target->{title, shortTitle, slug, _id}
+            }
+          }
+        }
+      }
+  }`
+  const order = `| order(updatedAt asc)`
+  const query = [filter, projection, order].join(' ')
+  const docs = await client.fetch(query).catch(err => console.error(err))
+  const reducedDocs = overlayDrafts(hasToken, docs)
+  const prepareMenu = reducedDocs.map(generateMenu)
+    console.log('--- prepareMenu ---------------------');
+  console.log('prepareMenu',prepareMenu);
+  console.log('-------------------------------');
+
+  return prepareMenu
+}
+
+module.exports = navigationTree
