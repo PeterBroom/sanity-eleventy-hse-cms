0eb0355dcad165a582610f39c9fdd9575cb5fa1d
Peter Broom <me@peterbroom.co.uk>
2021-05-24 10:40:37 +0100

    refactor form widths

--- studio/schemas/objects/pageBuilder/forms/width.js
+++ studio/schemas/objects/pageBuilder/forms/width.js
@@ -10,16 +10,15 @@ export default {
             type: 'string',
             options: {
             list: [
-                { title: 'Default', value: ''},
-                { title: 'Small input', value: 'input-width-s'},
-                { title: 'Medium input', value: 'input-width-m'},
-                { title: 'Large input', value: 'input-width-l'},
-                { title: 'Half width input', value: 'input-width-half'},
-                { title: 'Full width input', value: 'input-width-full'},
-                { title: '1 digit', value: 'input-digits-1'},
-                { title: '2 digits', value: 'input-digits-2'},
-                { title: '3 digits', value: 'input-digits-3'},
-                { title: '4 digits', value: 'input-digits-4'},
+                    { title: 'Default', value: ''},
+                    { title: 'Small input', value: 'input-width-s'},
+                    { title: 'Medium input', value: 'input-width-m'},
+                    { title: 'Large input', value: 'input-width-l'},
+                    { title: 'Full width input', value: 'input-width-full'},
+                    { title: '1 digit', value: 'input-digits-1'},
+                    { title: '2 digits', value: 'input-digits-2'},
+                    { title: '3 digits', value: 'input-digits-3'},
+                    { title: '4 digits', value: 'input-digits-4'}
                 ]
             }
         }
