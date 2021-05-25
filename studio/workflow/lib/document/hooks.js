9f00c4bfa548922f17615d5bdfe990e10d7a0fa0
Peter Broom <me@peterbroom.co.uk>
2021-05-25 12:32:19 +0100

    remove console.log

--- studio/workflow/lib/document/hooks.js
+++ studio/workflow/lib/document/hooks.js
@@ -2,7 +2,6 @@ import {getLatestDocumentPreview$} from './preview'
 import {useObservable} from '../utils/use'
 
 export function useLatestDocumentPreview (id, typeName) {
-  console.log('HERE HERE HERE')
   const latestPreview$ = getLatestDocumentPreview$(id, typeName)
 
   return useObservable(latestPreview$, null, [id, typeName])
