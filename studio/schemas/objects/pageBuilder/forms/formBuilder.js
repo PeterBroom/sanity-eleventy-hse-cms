e1b01dbeef9ed19ced5c5a7fbf3ba277aec9879e
Peter Broom <me@peterbroom.co.uk>
2021-05-25 10:36:57 +0100

    add fieldsets

--- studio/schemas/objects/pageBuilder/forms/formBuilder.js
+++ studio/schemas/objects/pageBuilder/forms/formBuilder.js
@@ -2,21 +2,50 @@ export default {
     name: 'formBuilder',
     title: 'Form builder',
     type: 'object',
+    fieldsets: [
+        {name: 'formActions', title: 'Form actions'},
+        {name: 'formCopy', title: 'Form copy'},
+        {name: 'formLayout', title: 'Form layout'},
+        {name: 'formFieldsets', title: 'Add fieldsets'}
+    ],
     fields: [
         {
             name: 'action',
             title: 'Form action',
-            type: 'url'
+            type: 'url',
+            fieldset: 'formActions'
+        },
+        {
+            name: 'autocomplete',
+            title: 'Auto complete',
+            type: 'boolean',
+            description: 'Specifies whether a field should have autocomplete enabled',
+            fieldset: 'formActions'
+        },
+        {
+            name: 'title',
+            title: 'Title',
+            type: 'string',
+            fieldset: 'formCopy'
+        },
+        {
+            name: 'introductoryText',
+            title: 'Introductory text',
+            type: 'portableText',
+            fieldset: 'formCopy'
         },
         {
             name: 'grid',
             title: 'Grid',
-            type: 'gridLayout'
+            type: 'gridLayout',
+            fieldset: 'formLayout'
         },
         {
             name: 'fields',
             title: 'Field set',
             type: 'array',
+            description: 'Add fieldsets so you can begin adding fields',
+            fieldset: 'formFieldsets',
             of: [
                 {
                   type: 'fieldset'
@@ -24,5 +53,4 @@ export default {
             ]
         }
     ]
-
 }
