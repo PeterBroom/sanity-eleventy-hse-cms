810ccd6daa93dc55f1b6b35ff77c354603040eb9
Peter Broom <me@peterbroom.co.uk>
2021-05-26 12:33:30 +0100

    add api version

--- web/client-config.js
+++ web/client-config.js
@@ -2,6 +2,7 @@ module.exports = {
   sanity: {
     projectId: process.env.SANITY_PROJECT_ID || 'nfc7ty44',
     dataset: process.env.SANITY_DATASET || 'production',
+    apiVersion: '2021-03-25',
     withCredentials: true
   }
 }
