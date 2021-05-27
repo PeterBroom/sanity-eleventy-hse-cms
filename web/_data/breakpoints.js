8c7fd0bcee0a90a0c269c15736422ca8e919a675
Peter Broom <me@peterbroom.co.uk>
2021-05-27 12:11:13 +0100

    add breakpoints

---
+++ web/_data/breakpoints.js
@@ -0,0 +1,9 @@
+function breakpoints() {
+    const dimensions = {
+      desktop: "980",
+      tablet: "800",
+      mobile: "600",
+    };
+    return dimensions;
+}
+module.exports = breakpoints;
